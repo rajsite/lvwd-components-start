850f544cd4c53a9538679db07fcc11fb24793aeb
rajsite <rajsite@gmail.com>
2015-09-20 19:12:42 -0500

    Change to iron-form until ajax-form supports Polymer 1.0
    Polymer 1.0 changes cause an unexpected change in resulting json in
    ajax-form: https://github.com/rnicholus/ajax-form/issues/73
    
    Switching to iron-form until the issue is resolved

--- web/dev/js/start.js
+++ web/dev/js/start.js
@@ -1,15 +1,13 @@
 /*jslint browser: true, devel: true, white: true*/
 
 // Make sure the Web Components are loaded before trying to use them
-// We have to use a polymer specific function for now due to limitations of the polyfills
 window.addEventListener('WebComponentsReady', function () {
     'use strict';
     
-    // Declare all variables at the start of the function
+    // Make references to the DOM elements we want to use
     var addForm  = document.getElementById('add-form'),
         addSubmit  = document.getElementById('add-submit'),
-        addResult = document.getElementById('add-result'),
-        data;
+        addResult = document.getElementById('add-result');
     
     // Paper-buttons don't behave like <button type="submit"> so we have to listen for the click ourselves
     addSubmit.addEventListener('click', function () {
@@ -17,18 +15,26 @@ window.addEventListener('WebComponentsReady', function () {
     });
     
     // When ajax-form recieves a response from the server we can parse it
-    addForm.addEventListener('submitted', function (evt) {
+    addForm.addEventListener('iron-form-response', function (evt) {
+        addResult.value = evt.detail.sum;
+    });
+    
+    addForm.addEventListener('iron-form-error', function (evt) {
+        var response;
         
-        // On a 200 OK response show the resulting sum in the field with id 'add-result'
-        if (evt.detail.status === 200) {
-            data = JSON.parse(evt.detail.responseText);
-            addResult.value = data.sum;
-        }
-        
-        // Otherwise show an error to the user and log response to browser console
-        else {
-            addResult.value = 'Error: ' + evt.detail.statusText;
-            console.log('Error: ' + evt.detail.responseText);
+        // Handle error codes we have built into the LabVIEW application
+        if (evt.detail.request.xhr.status === 500 && evt.detail.request.xhr.responseType === 'json') {
+            response = evt.detail.request.xhr.response;
+            
+            addResult.value = response.message;
+            console.log('LabVIEW Server Error Message: ', response.message);
+            console.group();
+            console.log('LabVIEW Server Error Status: ', response.error.status);
+            console.log('LabVIEW Server Error Code: ', response.error.code);
+            console.log('LabVIEW Server Error Source: ', response.error.source);
+            console.groupEnd();
+        } else {
+            addResult.value = evt.detail.error.message;
         }
     });
 });
